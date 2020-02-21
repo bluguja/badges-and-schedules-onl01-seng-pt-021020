9d5a5a2afc066e170e982c57c41494db2c094318
Bala Luguja <bluguja@gmail.com>
2020-02-21 10:14:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,10 +15,16 @@ end
   new_arr
 end
 
-def assign_rooms(attendees)
+def assign_rooms(arr_of_names)
+  counter=0 
+  arr_of_names.map do |attendee|
+  "Hello, #{attendee}! You'll be assigned to room #{counter += 1}!"
+    
+  end
   
 end
 
 def printer(attendees)
+  
   
 end
