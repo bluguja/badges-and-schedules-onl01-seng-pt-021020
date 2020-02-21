f9b6468930a723d4dd6109bf11c6a20951406fc7
Bala Luguja <bluguja@gmail.com>
2020-02-21 09:26:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
+require 'pry'
 # Write your code here.
+
+def badge_maker(str)
+  binding.pry
+  "Hello, my name is #{str}"
+  
+end
+
+def batch_badge_creator(attendees)
+  
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+  
+end
